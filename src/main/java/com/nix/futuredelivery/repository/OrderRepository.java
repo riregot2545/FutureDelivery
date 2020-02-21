4ac0ca7e7b453bd9c5f91845d1e100ce6868ea8c
dimas307 <dimus307@gmail.com>
2020-02-21 20:18:07 +0200

    Add Jpa to interfaces upd

--- src/main/java/com/nix/futuredelivery/repository/OrderRepository.java
+++ src/main/java/com/nix/futuredelivery/repository/OrderRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
+
+
 }
