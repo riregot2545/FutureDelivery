ddc5da10079e596b84fed1bbd795a1dd1de9bb9f
riregot2545 <dima4elom@gmail.com>
2020-03-07 17:56:23 +0200

    Scheduling for transportation distributions added

---
+++ src/main/java/com/nix/futuredelivery/service/ScheduledTransportationWorker.java
@@ -0,0 +1,24 @@
+package com.nix.futuredelivery.service;
+
+import com.nix.futuredelivery.transportation.TransportationProcessor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ScheduledTransportationWorker {
+    private final TransportationProcessor transportationProcessor;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void distributeStoreOrders() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.YYYY HH:mm:ss");
+        log.info("Time is now {}, starting order distribution...", dateFormat.format(new Date()));
+        transportationProcessor.proceedOrders();
+    }
+}
