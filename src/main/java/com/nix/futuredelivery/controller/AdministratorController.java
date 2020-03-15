8a3ea3d324df6e7aab730bfbc2f3b2653b4859da
timstuf <nastushagrishaeva@gmail.com>
2020-03-15 14:24:33 +0200

    mcompleted warehouse, store, security

--- src/main/java/com/nix/futuredelivery/controller/AdministratorController.java
+++ src/main/java/com/nix/futuredelivery/controller/AdministratorController.java
@@ -1,8 +1,6 @@
 package com.nix.futuredelivery.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("hasAuthority('ADMIN')")
 public class AdministratorController {
 
-    @GetMapping("try")
-    public String hasAccess(Authentication authentication){
-        return "yess";
-    }
 }
